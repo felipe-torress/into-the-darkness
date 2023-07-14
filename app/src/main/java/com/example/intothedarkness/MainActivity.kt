e2553aec9a2458f648db35e955de5d5057a7cb70
fetl9239 <fetl9239@ufmg.br>
2023-07-13 23:47:02 -0300

    feat: add button to center of application

--- app/src/main/java/com/example/intothedarkness/MainActivity.kt
+++ app/src/main/java/com/example/intothedarkness/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.intothedarkness
 
+import android.icu.text.CaseMap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.intothedarkness.ui.theme.IntoTheDarknessTheme
+import com.example.intothedarkness.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntoTheDarknessTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
                 }
             }
         }
@@ -27,14 +33,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(80.dp)
+        ) {
+            Text(text = "Click Here")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IntoTheDarknessTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
