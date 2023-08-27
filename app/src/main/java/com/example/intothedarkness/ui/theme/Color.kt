2c3893aeb8d045d048c2a96dff771901f44e14c2
Felipe Torres <fetl9239@ufmg.br>
2023-08-26 23:12:44 -0300

    feat: add background colors and make navbar responsive to background color

--- app/src/main/java/com/example/intothedarkness/ui/theme/Color.kt
+++ app/src/main/java/com/example/intothedarkness/ui/theme/Color.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFd0bcff)
 val PurpleGrey80 = Color(0xFFccc2dc)
 val Pink80 = Color(0xFFefb8c8)
-val lightBackground = Color(0xFFffffff)
+val LightBackground = Color(0xFFede8f7)
 //endregion
 
 //region --- Dark Theme ---
@@ -15,4 +15,5 @@ val lightBackground = Color(0xFFffffff)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7d5260)
+val DarkBackground = Color(0xFF191b2a)
 //endregion
--- app/src/main/java/com/example/intothedarkness/ui/theme/Theme.kt
+++ app/src/main/java/com/example/intothedarkness/ui/theme/Theme.kt
@@ -12,32 +12,34 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80,
+    background = DarkBackground
 )
 
 private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40,
+    background = LightBackground
 
-        /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    /* Other default colors to override
+background = Color(0xFFFFFBFE),
+surface = Color(0xFFFFFBFE),
+onPrimary = Color.White,
+onSecondary = Color.White,
+onTertiary = Color.White,
+onBackground = Color(0xFF1C1B1F),
+onSurface = Color(0xFF1C1B1F),
+*/
 )
 
 @Composable
 fun IntoTheDarknessTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
-        // Dynamic color is available on Android 12+
-        content: @Composable () -> Unit
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         darkTheme -> DarkColorScheme
@@ -48,13 +50,16 @@ fun IntoTheDarknessTheme(
         SideEffect {
             (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+
+            (view.context as Activity).window.navigationBarColor = colorScheme.background.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightNavigationBars = !darkTheme
         }
     }
 
     MaterialTheme(
-            colorScheme = colorScheme,
-            typography = Typography,
-            content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
 
