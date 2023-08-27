cfc1301f67db1a3a1474e4e2e7c08d3e83d5c3ff
Felipe Torres <fetl9239@ufmg.br>
2023-08-27 00:04:30 -0300

    feat: change status bar color to match background color

--- app/src/main/java/com/example/intothedarkness/ui/theme/Theme.kt
+++ app/src/main/java/com/example/intothedarkness/ui/theme/Theme.kt
@@ -48,8 +48,8 @@ fun IntoTheDarknessTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !darkTheme
 
             (view.context as Activity).window.navigationBarColor = colorScheme.background.toArgb()
             ViewCompat.getWindowInsetsController(view)?.isAppearanceLightNavigationBars = !darkTheme
