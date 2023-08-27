42c320bc8c48edd36661df1622e4fd3e92b662ec
Felipe Torres <fetl9239@ufmg.br>
2023-08-26 22:43:26 -0300

    feat: change button text color according to theme

--- app/src/main/java/com/example/intothedarkness/ui/composables/ChangeThemeButton.kt
+++ app/src/main/java/com/example/intothedarkness/ui/composables/ChangeThemeButton.kt
@@ -4,12 +4,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -22,9 +20,6 @@ fun ChangeThemeButton(
 ) {
     Button(
         onClick = onChangeThemeClicked,
-        colors = ButtonDefaults.buttonColors(
-            contentColor = Color.White
-        ),
         modifier = Modifier
             .height(150.dp)
             .width(300.dp),
