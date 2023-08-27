ea21c6fbbbb9032e147c377b06242189032da068
Felipe Torres <fetl9239@ufmg.br>
2023-08-26 22:50:35 -0300

    feat: organize color variables into regions and standardize hex color formatting

--- app/src/main/java/com/example/intothedarkness/ui/theme/Color.kt
+++ app/src/main/java/com/example/intothedarkness/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.example.intothedarkness.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//region --- Light Theme ---
+/** Light Theme Colors */
+val Purple80 = Color(0xFFd0bcff)
+val PurpleGrey80 = Color(0xFFccc2dc)
+val Pink80 = Color(0xFFefb8c8)
+val lightBackground = Color(0xFFffffff)
+//endregion
 
+//region --- Dark Theme ---
+/** Light Theme Colors */
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFF7d5260)
+//endregion
