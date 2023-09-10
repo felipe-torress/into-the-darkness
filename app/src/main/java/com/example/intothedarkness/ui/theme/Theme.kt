c97ddc774e98aca171963212291369b99ca41ca5
Felipe Torres <fetl9239@ufmg.br>
2023-09-10 14:34:47 -0300

    fix: removing unused lines of code

--- app/src/main/java/com/example/intothedarkness/ui/theme/Theme.kt
+++ app/src/main/java/com/example/intothedarkness/ui/theme/Theme.kt
@@ -23,22 +23,11 @@ private val LightColorScheme = lightColorScheme(
     secondary = PurpleGrey40,
     tertiary = Pink40,
     background = LightBackground
-
-    /* Other default colors to override
-background = Color(0xFFFFFBFE),
-surface = Color(0xFFFFFBFE),
-onPrimary = Color.White,
-onSecondary = Color.White,
-onTertiary = Color.White,
-onBackground = Color(0xFF1C1B1F),
-onSurface = Color(0xFF1C1B1F),
-*/
 )
 
 @Composable
 fun IntoTheDarknessTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -62,7 +51,3 @@ fun IntoTheDarknessTheme(
         content = content
     )
 }
-
-//private val IntoTheDarknessColorScheme: ColorScheme = ColorScheme(
-//
-//)
